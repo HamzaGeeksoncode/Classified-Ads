f694c0750af800bcd26e8f17f3044ebc2b9abb8f
HamzaGeeksoncode <hamza.mehood@appiskey.com>
2023-02-25 05:05:09 +0500

    RoleSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             AdModelSeeder::class,
+            RoleSeeder::class
         ]);
     }
 }
